80c57792f3b4cbf44719ec9905e3823754b8e621
HisonEdc <38303047+HisonEdc@users.noreply.github.com>
2019-03-31 22:08:37 +0800

    Update PaperModel_v1.0.py

--- PaperModel_v1.0.py
+++ PaperModel_v1.0.py
@@ -20,7 +20,7 @@ p = 100
 
 # get MNIST dataset
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets('/Users/hejiaxin/PycharmProjects/MNIST_data',one_hot=True)
+mnist = input_data.read_data_sets('..../MNIST_data',one_hot=True)
 
 # count the sample number of each datasets
 train_nums = mnist.train.num_examples
